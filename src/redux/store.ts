30231306b53127f780e6b681bf1e144677d9fa9f
Md. Shah Alam <shahalam141072@gmail.com>
2026-06-14 11:17:21 +0600

    :pencil2: => Learning about middleware of redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,13 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const myLooger = (store: any) => (next: any) => (action: any) => {
+    console.log(`Previous state is ${JSON.stringify(store.getState().counter )}`);
+    console.log(`Next state is ${JSON.stringify(store.getState().counter)}`);
+
+    return next(action);
+}
+
+const store = createStore(rootReducer, applyMiddleware(myLooger));
 
 export default store;

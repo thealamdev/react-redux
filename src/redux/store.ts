9436f8f5965f3cb2e50a5e35acf3407af40a95d3
Md. Shah Alam <shahalam141072@gmail.com>
2026-06-15 12:04:01 +0600

    :pencil2: => Working with module 3 and complete 1st video and setup the project

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,18 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import rootReducer from "./rootReducer";
+import { createLogger } from 'redux-logger'
+
+const logger = createLogger();
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const myLooger = (store: any) => (next: any) => (action: any) => {
-    console.log(`Previous state is ${JSON.stringify(store.getState().counter )}`);
+    console.log(`Previous state is ${JSON.stringify(store.getState().counter)}`);
     console.log(`Next state is ${JSON.stringify(store.getState().counter)}`);
 
     return next(action);
 }
 
-const store = createStore(rootReducer, applyMiddleware(myLooger));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, myLooger)));
 
 export default store;

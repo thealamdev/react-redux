16e9a299c173068357c40146b80b38fd5f64e229
Md. Shah Alam <shahalam141072@gmail.com>
2026-06-16 21:43:48 +0600

    :up: => Filter by colors and filter by status done

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,13 +3,35 @@ import List from './List'
 import type { Todo } from '../redux/todos/initialValues'
 
 export default function ListItem() {
-    const todos = useSelector((state: any) => state.todos)
+    const todos = useSelector((state: any) => state.todos);
+    const filter = useSelector((state: any) => state.filters);
+
+    const filterByStatus = (item: any) => {
+        switch (filter.status) {
+            case 'Complete':
+                return item.completed
+            case 'Incomplete':
+                return !item.completed
+            default:
+                return true;
+        }
+    }
+
+    const filterByColors = (item: any) => {
+        const { colors } = filter;
+        if (colors.length > 0) {
+            return colors.includes(item?.color);
+        }
+        return true;
+    }
+
+    const filteredTodos = todos.filter(filterByStatus).filter(filterByColors)
     return (
         <div>
             <div
                 className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto"
             >
-                {todos.map((todo: Todo) => (
+                {filteredTodos?.map((todo: Todo) => (
                     <List todo={todo} key={todo.id} />
                 ))}
             </div>
